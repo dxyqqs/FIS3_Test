f88e598da57656c7ca3f1dea2006faba20f1a32e
dxyqqs <dxyqq@sian.com>
2016-07-06 14:38:52 +0800

    add files

---
+++ app/react/app.js
@@ -0,0 +1,3 @@
+var a = requir('./comp/1/1.js');
+
+console.log(a.value);
---
+++ fis-conf.js
@@ -0,0 +1,22 @@
+//fis config
+
+console.log(11111)
+
+// fis.hook('commonjs')
+//
+// fis.match('/copm/**/*.js',{
+//   isMod:true,
+//   release:'/static/$0'
+// })
+// fis.match('::package',{
+//   postpackage:fis.plugin('loader',{
+//     resourceType:'commonjs',
+//     useInlineMap:true
+//   })
+//
+// })
+// fis.media('prod')
+//    .match('*.js',{
+//      packTo:'/static/libs/main.js'
+//    })
+//
