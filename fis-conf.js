33028a4fc06c9ad0fb1a32fc7d322090f60b625d
dxyqqs <dxyqq@sian.com>
2016-07-06 15:20:17 +0800

    'modified'

--- fis-conf.js
+++ fis-conf.js
@@ -1,9 +1,19 @@
 //fis config
+var path = require('path');
+var init = require("./package.json");
+var projectPath = "/app/"+init.testPath+"/";
+console.log(projectPath);
 
-console.log(11111)
+// fis.set('projec.ignore',function(){
+//   var ignore = fis.get('projec.ignore');
+//   [].push.call(ignore,['.git/**','clone/**','dist/**','.gitignore']
+//   return ignore;
+// }());
 
-// fis.hook('commonjs')
-//
+fis.match("/app/("+init.testPath+"/**)",{
+  release:'$1'
+})
+//fis.hook('commonjs');
 // fis.match('/copm/**/*.js',{
 //   isMod:true,
 //   release:'/static/$0'
